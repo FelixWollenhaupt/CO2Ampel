26513ab2187a5adea09b4bb0922fe7014187f31b
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2022-01-18 15:12:36 +0100

    documentation & typing

--- rgb_controller.py
+++ rgb_controller.py
@@ -1,5 +1,4 @@
-
-
+from typing import Collection
 import pigpio
 from time import sleep
 
@@ -9,29 +8,36 @@ PIN_RED = 17
 PIN_GREEN = 22
 PIN_BLUE = 24
 
-def _ensure_valid_brightness(brightness):
+def _ensure_valid_brightness(brightness: int) -> int:
+    """clamps the parameter brightness to the range 0 - 255"""
     if brightness > 255:
         return 255
     elif brightness < 0:
         return 0
     return brightness
 
-def _lerp(a, b, amt):
+def _lerp(a: float, b: float, amt: float) -> float:
+    """linearly interpolates between a and b by the amount amt"""
     return a * (1 - amt) + b * amt 
 
-def _lerp_color(color_a, color_b, amt):
+def _lerp_color(color_a: Collection, color_b: Collection, amt: float) -> Collection:
+    """interpolates between two given colors by the amount amt"""
     ret = [_lerp(a, b, amt) for a, b in zip(color_a, color_b)]
     return ret
 
-def set_color(color):
+def set_color(color: Collection) -> None:
+    """sets leds to the given color"""
     pi.set_PWM_dutycycle(PIN_RED, _ensure_valid_brightness(color[0]))
     pi.set_PWM_dutycycle(PIN_GREEN, _ensure_valid_brightness(color[1]))
     pi.set_PWM_dutycycle(PIN_BLUE, _ensure_valid_brightness(color[2]))
 
-def clear():
+def clear() -> None:
+    """clears leds"""
     set_color((0, 0, 0))
 
-def set_ampel(amount):
+def set_ampel(amount: float) -> None:
+    """lights up leds on a scale from green to red according to the parameter amount.
+        0 -> green, 1 -> red"""
     if amount > 1:
         raise ValueError("Ampel amount above 1")
     elif amount < 0:
@@ -39,16 +45,16 @@ def set_ampel(amount):
 
     set_color(_lerp_color((0, 255, 0), (255, 0, 0), amount))
 
-if __name__ == "__main__":
-    for i in range(100):
-        l = i / 100
-        set_ampel(l)
-        sleep(0.05)
-
-    sleep(1)
-
+def quit() -> None:
+    """clears the leds and stops the controll"""
     clear()
-
     pi.stop()
 
+
+if __name__ == "__main__":
+    set_ampel(0.8)
+    
+    sleep(2)
+
+    quit()
 
